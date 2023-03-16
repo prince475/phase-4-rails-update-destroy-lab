d579d5056da54cc90b2d38fcd16d17c755ce1ed5
prince475 <danielmwai475@gmail.com>
2023-03-16 23:12:25 +0300

    Completed the update and destroy routes and handled exceptions

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +19,34 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCHASE /plants/:id/update
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+
+  end
+
+  # DELETE /plant/:id/delete
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  # exception handling our error response
+  def render_not_found_response
+    render json: { error: "plant not found"  }, status: :not_found
+  end
+
+  # using find
+  def find_plant
+    Plant.find(params[:id])
   end
 end
